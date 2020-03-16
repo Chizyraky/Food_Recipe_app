d446d90fd6de39ae73ec834ae9f4cf4868b380fe
Chizoba <chizyraky@gmail.com>
2020-03-16 22:41:00 +0200

    Created java project

---
+++ src/main/java/chizy/foodrecipeapp/FoodRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package chizy.foodrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodRecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chizy/foodrecipeapp/FoodRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package chizy.foodrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
